0ed449c27d284e892a4d7f7cb854920c4b3c5f6e
kjuulh <contact@kjuulh.io>
2022-10-21 12:03:17 +0200

    updated formula

--- pkg/brew/kignore-bin.rb
+++ pkg/brew/kignore-bin.rb
@@ -1,16 +1,18 @@
 class KignoreBin < Formula
   desc "Easily add items to .gitignore and cleanup afterwards"
   homepage "https://github.com/kjuulh/gitignore"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   if OS.mac?
     url "https://github.com/kjuulh/gitignore/releases/download/#{version}/kignore-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "8eacb5cf0d4116161f291006bc6c91b8ce2760eab0fcf8cb79a62c246bdfff89"
+    sha256 "fa4e520854f0cc8222625b0398c778d4f474dd7a9ad1da1dd9a326ff7893bd44"
   end
 
   def install
     bin.install "kignore"
+    bin.install "git-ignore"
+    bin.install "git-ignore" => git-kignore
   end
 end
 
